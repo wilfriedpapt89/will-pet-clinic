7c805078f2e44ceac7ec99293c618a9807979df1
wilfriedpapt89 <wilfriedpapt@gmail.com>
2021-08-04 18:59:07 +0200

    Adding owners index page and controller. CLoses #13

--- pet-clinic-web/src/main/java/com/willpapt/willpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/willpapt/willpetclinic/controllers/OwnerController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class VetController {
+@RequestMapping({"/owners"})
+public class OwnerController {
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
-        return "vets/index";
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listOwners(){
+        return "owners/index";
     }
 }
