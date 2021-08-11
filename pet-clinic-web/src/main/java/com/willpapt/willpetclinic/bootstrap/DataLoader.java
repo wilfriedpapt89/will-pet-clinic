f386a36807d65db5f0c539f50fbb9e6e7bbbd14a
wilfriedpapt89 <wilfriedpapt@gmail.com>
2021-08-11 13:35:13 +0200

    Added PetType bootstrap data. Closes #32

--- pet-clinic-web/src/main/java/com/willpapt/willpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/willpapt/willpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.willpapt.willpetclinic.bootstrap;
 
 import com.willpapt.willpetclinic.model.Owner;
+import com.willpapt.willpetclinic.model.PetType;
 import com.willpapt.willpetclinic.model.Vet;
 import com.willpapt.willpetclinic.services.OwnerService;
+import com.willpapt.willpetclinic.services.PetTypeService;
 import com.willpapt.willpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        petTypeService.save(cat);
+
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
