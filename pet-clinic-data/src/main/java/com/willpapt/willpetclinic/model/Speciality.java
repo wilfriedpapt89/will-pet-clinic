d7d779dedd74c28275e6ff65352458479b3f7fc9
wilfriedpapt89 <wilfriedpapt@gmail.com>
2021-11-01 18:23:37 +0200

    Updated Vet and Speciality class with entity mappings. Closes #37

--- pet-clinic-data/src/main/java/com/willpapt/willpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/willpapt/willpetclinic/model/Speciality.java
@@ -1,5 +1,12 @@
 package com.willpapt.willpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity{
 
     private String description;
