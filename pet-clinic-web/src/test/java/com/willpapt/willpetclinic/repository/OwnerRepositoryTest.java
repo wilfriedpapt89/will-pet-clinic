1bfb1ae73ce832e6052af304ace799cd693c2a46
wilfriedpapt89 <wilfriedpapt@gmail.com>
2021-11-17 19:55:52 +0200

    Updated to Junit5. Closes #4

---
+++ pet-clinic-web/src/test/java/com/willpapt/willpetclinic/repository/OwnerRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.willpapt.willpetclinic.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+}
